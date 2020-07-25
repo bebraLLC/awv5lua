d2da92d395fcb28934257fedbfb1389d869f7c11
KriZz87 <32612882+ticzz@users.noreply.github.com>
2020-07-25 04:25:36 +0200

    Add files via upload

--- DeadESP.lua
+++ DeadESP.lua
@@ -25,36 +25,36 @@ local function DeadESP( )
 w, h = ScreenSize
 
 	if not player then
-			return
+		return
 	end		
 
 	if player:IsAlive() == false then										-- Dead and spectating someone
 
-            set("esp.overlay.enemy.name", true)
-            set("esp.chams.enemy.occluded", deadesp_chams_while_spec:GetValue())
+			set("esp.overlay.enemy.name", true)
+			set("esp.chams.enemy.occluded", deadesp_chams_while_spec:GetValue())
 			set("esp.chams.enemy.visible", "2")
 			set("esp.chams.enemy.overlay", "1")
 			set("esp.chams.enemyattachments.occluded", false)
 			set("esp.chams.enemyattachments.visible", false)
 			set("esp.chams.enemyattachments.overlay", false)
-            set("esp.chams.friendlyattachments.occluded", false)
+			set("esp.chams.friendlyattachments.occluded", false)
 			set("esp.chams.friendlyattachments.visible", false)
 			set("esp.chams.friendlyattachments.overlay", false)
 			set("esp.overlay.enemy.scoped", false)
-            set("esp.overlay.enemy.reloading", false)
-            set("esp.overlay.enemy.health.healthnum", false)
-            set("esp.overlay.enemy.health.healthbar", true)
+			set("esp.overlay.enemy.reloading", false)
+			set("esp.overlay.enemy.health.healthnum", false)
+			set("esp.overlay.enemy.health.healthbar", true)
 			set("esp.overlay.enemy.weapon", "1")
-            set("esp.overlay.enemy.box", false)
-            set("esp.overlay.enemy.hasdefuser", true)
-            set("esp.overlay.enemy.hasc4", true)
-            set("esp.overlay.enemy.barrel", false)
+			set("esp.overlay.enemy.box", false)
+			set("esp.overlay.enemy.hasdefuser", true)
+			set("esp.overlay.enemy.hasc4", true)
+			set("esp.overlay.enemy.barrel", false)
 			set("esp.overlay.enemy.armor", "1")
 
 	elseif player:IsAlive() == true then								-- Alive
             
 			set("esp.overlay.enemy.name", false)
-            set("esp.chams.enemy.occluded", false)
+			set("esp.chams.enemy.occluded", false)
 			set("esp.chams.enemy.visible", "2")
 			set("esp.chams.enemy.overlay", false)
 			set("esp.chams.enemyattachments.occluded", false)
@@ -63,26 +63,25 @@ w, h = ScreenSize
 			set("esp.chams.friendlyattachments.occluded", false)
 			set("esp.chams.friendlyattachments.visible", false)
 			set("esp.chams.friendlyattachments.overlay", false)
-            set("esp.overlay.enemy.scoped", false)
-            set("esp.overlay.enemy.reloading", false)
-            set("esp.overlay.enemy.health.healthnum", false)
-            set("esp.overlay.enemy.health.healthbar", false)
-            set("esp.overlay.enemy.weapon", false)
-            set("esp.overlay.enemy.box", false)
-            set("esp.overlay.enemy.hasdefuser", false)
-            set("esp.overlay.enemy.hasc4", false)
+			set("esp.overlay.enemy.scoped", false)
+			set("esp.overlay.enemy.reloading", false)
+			set("esp.overlay.enemy.health.healthnum", false)
+			set("esp.overlay.enemy.health.healthbar", false)
+			set("esp.overlay.enemy.weapon", false)
+			set("esp.overlay.enemy.box", false)
+			set("esp.overlay.enemy.hasdefuser", false)
+			set("esp.overlay.enemy.hasc4", false)
 			set("esp.overlay.weapon.ammo", false)
-            set("esp.overlay.enemy.barrel", false)
+			set("esp.overlay.enemy.barrel", false)
 			set("esp.overlay.enemy.armor", false)
 	end
 	
 	if (deadesp_wallhack_key:GetValue() ~= nil or false) then		
 	
 	if player:IsAlive() == true and input.IsButtonDown(deadesp_wallhack_key:GetValue()) then		-- Alive, holding down the wh-key
-				set("esp.chams.enemy.occluded", deadesp_chams_on_tggl:GetValue())
-
-				color(wh_chams_indicators_clr:GetValue())
-				text(15, h/2, "Wallhack Chams OnKey")
+			set("esp.chams.enemy.occluded", deadesp_chams_on_tggl:GetValue())
+			color(wh_chams_indicators_clr:GetValue())
+			text(15, h/2, "Wallhack Chams OnKey")
 			
 		end			
 	end
