45de6cb5d427385fb00320a6f8187ec9e174368d
KriZz87 <32612882+ticzz@users.noreply.github.com>
2020-08-29 22:56:46 +0200

    Add files via upload

--- Watermark_OneTap.lua
+++ Watermark_OneTap.lua
@@ -1,30 +1,3 @@
-----    Base    code    for    auto    updating.
---
---local    cS    =    GetScriptName()
---local    cV    =    '1.0.0'
---local    gS    =    'PUT    LINK    TO    RAW    LUA    SCRIPT'
---local    gV    =    'PUT    LINK    TO    RAW    VERSION'
---
---local    function    AutoUpdate()
---	if    gui.GetValue('lua_allow_http')    and    gui.GetValue('lua_allow_cfg')    then
---		local    nV    =    http.Get(gV)
---		if    cV    ~=    nV    then
---			local    nF    =    http.Get(gS)
---			local    cF    =    file.Open(cS,    'w')
---			cF:Write(nF)
---			cF:Close()
---			print(cS,    'updated    from',    cV,    'to',    nV)
---		else
---			print(cS,    'is    up-to-date.')
---		end
---	end
---end		
---
---callbacks.Register('Draw',    'Auto    Update')
---callbacks.Unregister('Draw',    'Auto    Update')
-
-
-
 local font = draw.CreateFont('Arial', 14);
 local windowW, windowH, menuPressed, j = 240, 440, 1, 0;
 local mainWindow = gui.Window("rab_onetap_watermark", "Onetap watermark", 200, 200, windowW, windowH);
