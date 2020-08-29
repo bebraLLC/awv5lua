a71abd42918c1bf29e024b6b3809fdd88fc4262b
KriZz87 <32612882+ticzz@users.noreply.github.com>
2020-08-29 23:15:11 +0200

    Update and rename EngineRadar&FPSstuff.lua to EngineRadar&FPSboost.lua

--- EngineRadar&FPSboost.lua
+++ EngineRadar&FPSboost.lua
@@ -317,18 +317,22 @@ end
 callbacks.Register('Draw', "disable_panorama_blur", disable_panorama_blur)
 
 -- Engine Radar by Nyanpasu!
+local vis_tab_radar = gui.Reference('VISUALS')
+local vis_main_tab = gui.Tab(vis_tab_radar, "lua_tab_radar", "EngineRadar")
+local EngineRadarchk = gui.Checkbox ( vis_main_tab, "lua_engine_radar", "Enable EngineRadar", false );
+
+
 local function engine_radar_draw()
+
+if EngineRadarchk:GetValue() then        
 
 for index, Player in pairs(entities.FindByClass("CCSPlayer")) do
 
-if not EngineRadarchk:GetValue() then        
-
-Player:SetProp("m_bSpotted", 0);
-
+Player:SetProp("m_bSpotted", 1);
 
 else
 
-Player:SetProp("m_bSpotted", 1);
+Player:SetProp("m_bSpotted", 0);
 
 end
 end
@@ -383,4 +387,4 @@ callbacks.Register ("FireGameEvent", eventz)
 
 
 
-
+
